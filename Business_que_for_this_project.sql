a037bbaff37418e4e8a7c70707d7d943d03e4ee5
Dharmesh Makwana <108424544+dharmesh9@users.noreply.github.com>
2026-02-13 19:59:43 +0000

    Add files via upload
    Business question for this dataset.

---
+++ Business_que_for_this_project.sql
@@ -0,0 +1,29 @@
+--Q1. Find the top 10 best-value products based on the discount percentage.
+
+--Q2.What are the Products with High MRP but Out of Stock
+
+--Q3.Calculate Estimated Revenue for each category
+
+--Q4. Find all products where MRP is greater than ₹500 and discount is less than 10%.
+
+--Q5. Identify the top 5 categories offering the highest average discount percentage.
+
+--Q6. Find the price per gram for products above 100g and sort by best value.
+
+--Q7.Group the products into categories like Low, Medium, Bulk.
+
+--Q8.What is the Total Inventory Weight Per Category 
+
+--Q9. Find the top 10 products generating the highest estimated revenue (mrp × availableQuantity).
+
+--Q10. Identify products where the discounted selling price is more than 30% lower than MRP.
+
+--Q11. Determine the total number of out‑of‑stock products in each category.
+
+--Q12. Find the average weight of products per category and sort by heaviest categories.
+
+--Q13. List products where the discountPercent is NULL or missing (data quality check).
+
+--Q14. Identify categories where the total available quantity is below 500 (low‑inventory categories).
+
+--Q15. Find products where the price per gram (discountedSellingPrice / weightInGms) is above the overall average price per gram.
